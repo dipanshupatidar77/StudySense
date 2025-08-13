eb99fa8dc54cd8374f93ce8254ac87ebd9195646
dipanshupatidar77 <dipanshupatidar77@gmail.com>
2025-08-13 22:11:38 +0530

    utils folder add

---
+++ client/src/utils/boilerplates.js
@@ -0,0 +1,11 @@
+export const boilerplates = {
+  '54': `#include <iostream>
+using namespace std;
+
+int main() {
+    // Your code here
+    return 0;
+}`,
+  '71': `print("Hello, World!")`,
+  '63': `console.log("Hello, World!");`,
+};
